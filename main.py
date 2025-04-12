a7a4be2f3b71f67cd2399f125664b584160b2191
Shyam Chandran <shyamchandranmec@gmail.com>
2025-04-12 22:37:43 +0530

    refactoring

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ load_dotenv(override=True)
 years = [2022, 2021, 2020, 2019]
 loader = UnstructuredReader()
 
-all_docs = []
 
 
 def load_all_docs() -> dict:
@@ -26,7 +25,6 @@ def load_all_docs() -> dict:
         for d in year_docs:
             d.metadata = {"year": year}
         doc_set[year] = year_docs
-        all_docs.extend(year_docs)
     return doc_set
 
 
